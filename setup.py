7fcdf5d43bd47838177f1a7b84d3a154c45c147f
1darshanpatil <patildarshan417@gmail.com>
2023-12-06 04:43:28 -0600

    markdown

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     license="MIT",
     description="PIN encrypted enigma machine cipher texts",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/1darshanpatil/enigmacipher", 
     packages=find_packages(),
     package_data={
